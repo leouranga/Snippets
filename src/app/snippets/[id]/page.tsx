804991464400ff328f1a4b3d8104e76182bd2af1
Leo Uranga <leo.uranga@hotmail.com>
2025-12-08 02:58:32 -0500

    update

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,5 +1,7 @@
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import { db } from "@/db";
+import * as actions from "@/actions";
 
 interface SnippetShowPageProps {
   params: Promise<{
@@ -17,5 +19,28 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
   if (!snippet) {
     return notFound();
   }
-  return <div>{snippet.title}</div>;
+
+  const deleteSnippetAction = actions.deleteSnippet.bind(null, snippet.id);
+
+  return (
+    <div>
+      <div className="flex m-4 justify-between items-center">
+        <h1 className="text-xl font-bold">{snippet.title}</h1>
+        <div className="flex gap-4">
+          <Link
+            href={`/snippets/${snippet.id}/edit`}
+            className="p-2 border rounded"
+          >
+            Edit
+          </Link>
+          <form action={deleteSnippetAction}>
+            <button className="p-2 border rounded">Delete</button>
+          </form>
+        </div>
+      </div>
+      <pre className="p-3 border rounded bg-gray-200 border-gray-200">
+        <code>{snippet.code}</code>
+      </pre>
+    </div>
+  );
 }
