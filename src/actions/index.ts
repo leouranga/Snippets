dc8bad8708f9a6d8c6b7d1e25961bd2ca7c80fbc
Leo Uranga <leo.uranga@hotmail.com>
2025-12-09 02:49:56 -0500

    update

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,4 +1,6 @@
 "use server";
+
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { db } from "@/db";
 
@@ -7,7 +9,7 @@ export async function editSnippet(id: number, code: string) {
     where: { id },
     data: { code },
   });
-
+  revalidatePath(`/snippets/${id}`);
   redirect(`/snippets/${id}`);
 }
 
@@ -16,5 +18,49 @@ export async function deleteSnippet(id: number) {
     where: { id },
   });
 
+  revalidatePath("/");
+  redirect("/");
+}
+
+export async function createSnippet(
+  formState: { message: string },
+  formData: FormData
+) {
+  try {
+    // Check the user's inputs and make sure they're valid
+    const title = formData.get("title");
+    const code = formData.get("code");
+
+    if (typeof title !== "string" || title.length < 3) {
+      return {
+        message: "Title must be longer",
+      };
+    }
+    if (typeof code !== "string" || code.length < 10) {
+      return {
+        message: "Code must be longer",
+      };
+    }
+    // Create a new record in the database
+    await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        message: err.message,
+      };
+    } else {
+      return {
+        message: "Something went wrong...",
+      };
+    }
+  }
+
+  revalidatePath("/");
+  // Redirect the user back to the root route
   redirect("/");
 }
